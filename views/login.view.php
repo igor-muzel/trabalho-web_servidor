bc30a29b0f7a6b110ba92175e7233c91a31f48c5
igor <igorcorinthians2008@gmail.com>
2024-10-27 17:28:57 -0300

    adicionado interface de login

---
+++ views/login.view.php
@@ -0,0 +1,11 @@
+<div class="login-container">
+    <h2>Login</h2>
+    <?php if (isset($error)): ?>
+        <p style="color: red;"><?php echo $error; ?></p>
+    <?php endif; ?>
+    <form action="login.php" method="POST">
+        <input type="text" name="username" placeholder="Usuário" required>
+        <input type="password" name="password" placeholder="Senha" required>
+        <button type="submit">Entrar</button>
+    </form>
+</div>
